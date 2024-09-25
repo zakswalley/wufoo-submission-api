79f34f33050e57c01526a06720f8937f6003cb9b
Zak Swalley <zacharydswalley@gmail.com>
2024-09-25 15:32:57 -0500

    Initial commit

---
+++ test/wufoo-submission-api.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const WufooSubmissionApi = require('../lib/wufoo-submission-api-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/wufoo-submission-api-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new WufooSubmissionApi.WufooSubmissionApiStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
